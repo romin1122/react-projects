1e708e7ef88efd9b60dc89805e6e44101a3b0911
Romin <muhymenul2003@gmail.com>
2024-07-08 23:45:14 +0600

    Project 3, reviews completed

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,44 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 
-
-const url = 'https://www.course-api.com/react-tours-project'
+const url = "https://www.course-api.com/react-tours-project";
 
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
 
-    const removeTour = ( id ) => {
-        setTours(prevTours => prevTours.filter(item => item.id != id));
-    }
+  const removeTour = (id) => {
+    setTours((prevTours) => prevTours.filter((item) => item.id != id));
+  };
 
-    const fetchTours = () => {
-        fetch(url)
-            .then((response)=>response.json())
-            .then((data) => {
-                setTours(data);
-                setLoading(false);
-            })
-            .catch(err => console.log(err));
-        console.log(url);
-    }
+  const fetchTours = () => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setTours(data);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+    console.log(url);
+  };
 
-    useEffect(() => {
-        fetchTours();
-    }, [])
+  useEffect(() => {
+    fetchTours();
+  }, []);
 
-    if (loading) return <Loading />;
-    else if (tours.length == 0) {
-        return (<main className='title'>
-                <h2>No Tours Left!</h2>
-                <button className='btn' onClick={fetchTours}>Refresh</button>
-            </main>)
-    }
-    else return (
-        <main>
-            <Tours tours={tours} removeTour={removeTour}/>;
-        </main>);
+  if (loading) return <Loading />;
+  else if (tours.length == 0) {
+    return (
+      <main className="title">
+        <h2>No Tours Left!</h2>
+        <button className="btn" onClick={fetchTours}>
+          Refresh
+        </button>
+      </main>
+    );
+  } else
+    return (
+      <main>
+        <Tours tours={tours} removeTour={removeTour} />;
+      </main>
+    );
 }
 
-export default App
+export default App;
--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -3,7 +3,52 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+    const [index, setIndex] = useState(0);
+    const {id, name, job, image, text} = people[index];
+
+    const prevIndex = () => {
+        let newIndex = index - 1;
+        if (newIndex < 0) newIndex = people.length - 1;
+        setIndex(newIndex);
+    }
+    const nextIndex = () => {
+        let newIndex = index + 1;
+        if (newIndex > people.length - 1) newIndex = 0;
+        setIndex(newIndex);
+    }
+    const randomIndex = () => {
+        let newIndex = Math.round( Math.random() * (people.length-1) );
+        if (newIndex === index) prevIndex();
+        setIndex(newIndex);
+    }
+
+
+    return (
+        <article className='review'>
+            <div className='img-container'>
+                <img src={image} alt={name} className='person-img'/>
+                <span className='quote-icon'>
+                    <FaQuoteRight />
+                </span>
+            </div>
+
+            <h4 className='author'>{name}</h4>
+            <p className='job'>{job}</p>
+            <p className='info'>{text}</p>
+
+            <div className='button-container'>
+                <button className='prev-btn' onClick={prevIndex}>
+                    <FaChevronLeft />
+                </button>
+                <button className='next-btn' onClick={nextIndex}>
+                    <FaChevronRight />
+                </button>
+            </div>
+            <button className='random-btn' onClick={randomIndex}>
+                Random
+            </button>
+        </article>
+    );
 };
 
 export default Review;
