133edfb1514165d92cb76b5d3826d5e4a37017d1
Romin <muhymenul2003@gmail.com>
2024-07-10 09:58:19 +0600

    Accordian(Np 4)Project done

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,15 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+
+    return <main>
+            <section className='container'>
+                <h3>Questions</h3>
+                <div>
+                    {data.map(question => <SingleQuestion key={question.id} data={question} />)}
+                </div>
+            </section>
+        </main>
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,16 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({data}) => {
+    const [expand, setExpand] = useState(false);
+    const toggleExpand = () => setExpand( prevState => !prevState );
+
+    return <div className='question'>
+            <header>
+                <h4>{data.title}</h4>
+                <button className='btn' onClick={toggleExpand}> {expand ? <AiOutlineMinus /> : <AiOutlinePlus />} </button>
+            </header>
+            { expand ? <p>{data.info}</p> : <> </> }
+        </div>
 };
 
 export default Question;
